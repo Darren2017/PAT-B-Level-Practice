070a8a6d2189ab9c2d267d50b94065dc6631c8d3
Darren2017 <33794339+Darren2017@users.noreply.github.com>
2017-12-22 23:32:25 +0800

    s
    s

---
+++ 1036.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    char ch;
+    scanf("%d %c", &N, &ch);
+    int i, j;
+    for(i =0; i < (N + 1) / 2; i++){
+        for(j = 0; j < N; j++){
+            if(i == 0 || i == (N + 1) / 2 - 1 ){
+                printf("%c", ch);
+            }else{
+                if(j == 0 || j == N - 1){
+                    printf("%c", ch);
+                }else{
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
