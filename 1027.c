b2389a41d113ff14f5540e75cf2b960fe2cfd6fc
Darren2017 <darren@tangaimindeMBP.lan>
2018-03-03 20:55:39 +0800

    s

---
+++ 1027.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n, m, r;
+    int p[25], q[25];
+    char s;
+    scanf("%d", &n);
+    getchar();
+    scanf("%c", &s);
+    m = sqrt((n + 1) / 2);
+    r = n - 2 * pow(m, 2) + 1;
+
+    for(int i = 0; i < m; i ++){
+        p[i] = 2 * (i + 1) - 1;
+        q[i] = 2 * (i + 1) - 1;
+    }
+    for(int i = m -1; i >=0 ; i--){
+        int j = m - i - 1;
+        while(j--){
+            putchar(' ');
+        }
+        while(p[i]--){
+            printf("%c", s);
+        }
+        printf("\n");
+    }
+    for(int i = 1; i < m; i++){
+        int j = m - i - 1;
+        while(j--){
+            putchar(' ');
+        }
+        while(q[i]--){
+            printf("%c", s);
+        }
+        printf("\n");
+    }
+
+    printf("%d", r);
+
+    return 0;
+
+}
