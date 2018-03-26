c4d737f9e6d19f28244f3c58351d3d90fab2cee6
Darren2017 <17362990052@163.com>
2018-03-26 08:46:10 +0800

    s

---
+++ 1038.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int N, K, gola;
+    int score[101] = {0};
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        scanf("%d", &gola);
+        score[gola]++;
+    }
+    scanf("%d", &K);
+    int m;
+    for(int i = 0; i < K; i ++){
+        scanf("%d", &m);
+        printf("%d", score[m]);
+        printf("%c", i < K - 1 ? ' ' : '\0');
+    }
+    
+    return 0;
+}
