fdcd270af8cc6a3e646f6485e1b512dda8489a98
Darren2017 <17362990052@163.com>
2018-03-29 00:38:22 +0800

    s

---
+++ 1049.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    double num[100001] = {0};
+    scanf("%d", &n);
+    int m = n;
+    double result = 0.0;
+    for(int i = 1; i <= n; i++){
+        scanf("%lf", &num[i]);
+        result = result + num[i] * 1.0 * m * i;
+        m--;
+    }
+    printf("%.2lf", result);
+    return 0;
+}
