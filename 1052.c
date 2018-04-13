e718c646e88a52fd2ee43e807d5563241b3247e7
Darren2017 <17362990052@163.com>
2018-04-13 13:55:16 +0800

    s

--- 1052.c
+++ 1052.c
@@ -1,27 +1,75 @@
 #include<stdio.h>
+#include<string.h>
 
 void handle(int a, char str[]);
 
 int main()
 {
-    char smp[3][44];
+    char c, s[3][10][5];
+    int cnt[3] = {0};
     for(int i = 0; i < 3; i++){
-        gets(smp[i]);
+        int j = 0;
+        while((c = getchar()) != '\n'){
+            if(c == '['){
+            scanf("%[^]]", s[i][j++]);
+            cnt[i]++;
+            }
+        }
     }
-    int n;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        int a1, a2, a3, a4, a5;
-        scanf("%d %d %d %d %d", &a1, &a2, &a3, &a4, &a5);
-        handle(a1, smp[0]);
-        handle(a2, smp[1]);
-        handle(a3, smp[2]);
-        handle(a4, smp[3]);
-        handle(a5, smp[4]);
+    printf("%d", cnt[1]);
+    int N, m;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        static char result[100] = "";
+        scanf("%d", &m);        //左手
+        if(m > 0 && m <= cnt[0]){
+            //printf("读取");
+            strcpy(result, s[0][m - 1]);
+            //printf("%s\n", result);
+        }else{
+            printf("Are you kidding me? @\\/@\n");
+            continue;
+        }
+        scanf("%d", &m);        //左眼
+        if(m > 0 && m <= cnt[1]){
+           // printf("读取");
+            strcat(result, "(");
+            strcat(result, s[1][m - 1]);
+            //printf("%s\n", result);
+        }else{
+            printf("Are you kidding me? @\\/@\n");
+            continue;
+        }
+        scanf("%d", &m);        //口
+        if(m > 0 && m <= cnt[2]){
+            //printf("读取");
+            strcat(result, s[2][m - 1]);
+            //printf("%s\n", result);
+        }else{
+            printf("Are you kidding me? @\\/@\n");
+            continue;
+        }
+        scanf("%d", &m);        //右眼
+        if(m > 0 && m <= cnt[1]){
+            //printf("读取");
+            strcat(result, s[1][m - 1]);
+            strcat(result, ")");
+            //printf("%s\n", result);
+        }else{
+            printf("Are you kidding me? @\\/@\n");
+            continue;
+        }
+        scanf("%d", &m);        //右手
+        if(m > 0 && m <= cnt[0]){
+            //printf("读取");
+            strcat(result, s[0][m - 1]);
+            //printf("%s\n", result);
+        }else{
+            printf("Are you kidding me? @\\/@\n");
+            continue;
+        }
+        printf("%s\n", result);
     }
-}
 
-void handle(int a, char str[]){
-    a--;
-    for(int i = 0)
+    return 0;
 }
