8e62b31dfb7e6a6fe432986e4207416c819252da
Darren2017 <17362990052@163.com>
2018-05-06 00:43:27 +0800

    s

--- 1074.c
+++ 1074.c
@@ -1,36 +1,37 @@
-#include<stdio.h>
-
-void dfunc(int i, int c[]){
-    for(int j = 0; j < i / 2; j++){
-        int temp = c[j];
-        c[j] = c[i - j - 1];
-        c[i - j - 1] = temp;
-    }
-}
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    int a, b, d, dd[20] = {0}, aa[20] = {0}, bb[20] = {0}, result[;
-    scanf("%d %d %d", &d, &a, &b);
-    int i = 0;
-    while(d){
-        dd[i++] = d % 10;
-        d /= 10;
-    }
-    i = 0;
-    while(a){
-        aa[i++] = a % 10;
-        a /= 10;
-    }
-    i = 0;
-    while(b){
-        bb[i++] = b % 10;
-        b /= 10;
-    }
+    int nBase, nA, nB, nS = 21, base, a, b;
+    char sBase[21] = {0}, sA[21] = {0}, sB[21] = {0}, sSum[22] = {0};
 
-    for(int j = 0; j < i; j++){
-
+    scanf("%s %s %s", sBase, sA, sB);
+    
+    nBase = strlen(sBase);
+    nA = strlen(sA);
+    nB = strlen(sB);
+    
+    for(int i = 0; i < nBase; i++)
+    {
+        a = nA <= i ? 0 : sA[nA - i - 1] - '0';
+        b = nB <= i ? 0 : sB[nB - i - 1] - '0';
+        base = sBase[nBase - i - 1] - '0';
+        base = base == 0 ? 10 : base;
+        sSum[nS - i - 1] += a + b;
+        sSum[nS - i - 2] += sSum[nS - i - 1] / base;
+        sSum[nS - i - 1] = sSum[nS - i - 1] % base;
     }
     
+    for(int i = 0; i < 21; i++)
+        sSum[i] += '0';
+    
+    for(int first = 0; first < 21; first++) 
+        if(sSum[first] != 0 && sSum[first] != '0')
+        {
+            puts(sSum + first);
+            return 0;
+        }
+    printf("0");
     return 0;
 }
