fb4f10cf16fb1702e4c32fd88dec93e80be9da1e
Darren2017 <17362990052@163.com>
2018-05-06 13:50:31 +0800

    S

---
+++ 1082.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int maxID, minID, maxDis = 0, minDis = 100000000, N, ID, x, y, distance;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        scanf("%d %d %d", &ID, &x, &y);
+        distance = x * x + y * y;
+        if(maxDis < distance){
+            maxDis = distance;
+            maxID = ID;
+        }
+        if(minDis > distance){
+            minDis = distance;
+            minID = ID;
+        }
+    }
+
+    printf("%04d %04d", minID, maxID);
+
+    return 0;
+}
