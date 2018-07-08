debdd0d6602829b7bcce56d3416c3b54f4fab814
Darren2017 <17362990052@163.com>
2018-07-08 14:19:13 +0800

    插入与归并

--- 1035.cpp
+++ 1035.cpp
@@ -1,50 +1,47 @@
-#include <iostream>
-#include <algorithm>
+#include<cstdio>
+#include<algorithm>
 using namespace std;
-
-void mergesort(int a[], int L, int R);
-void merger(int a[], int L, int M, int R);
-
 
 int main()
 {
-    int N, a[101], b[101], i, j;
-    cin >> N;
-    for(i = 0; i < N; i++){
-        cin >> a[i];
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int b[n];
+    for(int i=0 ;i<n ;i++){
+        scanf("%d",&a[i]);
     }
-    for(i = 0; i < N; i++){
-        cin >> b[i];
+    for(int i=0 ;i<n ;i++){
+        scanf("%d",&b[i]);
     }
-    for(i = 0; b[i] <= b[i+i] && i < N - 1; i++);
-    for(j = i + 1; a[j] == b[j] && j < N; j++);
-    if(j == N){
-        cout << "Insertion Sort" << endl;
-         sort(a, a + i + 2);
-        // int key = a[i + 1];
-        // for(int j = i; j > 0 && b[j - 1] > key; j--){
-        //     b[j] = b[j - 1];
-        // }
-        // b[j] = key;
+    int i,j;
+    for(i=0 ;i<n-1 && b[i+1]>=b[i] ;i++);
+    for(j=i+1 ;j<n && a[j]==b[j];j++);
+
+    if(j==n){
+        printf("Insertion Sort\n");
+        sort(a,a+i+2);
     }else{
-        cout << "Merge Sort" << endl;
-        int k = 1, flag = 1;
+        printf("Merge Sort\n");
+        int k = 1;
+        int flag = 1;
         while(flag) {
             flag = 0;
-            for (i = 0; i < N; i++) {
+            for (i = 0; i < n; i++) {
                 if (a[i] != b[i])
                     flag = 1;
             }
+
             k = k * 2;
-            for (i = 0; i < N / k; i++){
+            for (i = 0; i < n / k; i++){
                 sort(a + i * k, a + (i + 1) * k);
             }
-            sort(a + N / k * k, a + N);
+            sort(a + n / k * k, a + n);
         }
     }
-    for (j = 0; j < N; j++) {
-        if (j != 0) printf(" ");
-        printf("%d", a[j]);
+    for(int i=0 ;i<n-1 ;i++){
+        printf("%d ",a[i]);
     }
+    printf("%d\n",a[n-1]);
     return 0;
 }
